c9945fa6f39c4ba6fcc600bab0cd41868352af67
aniketz18 <aniketzade700@gmail.com>
2026-07-13 14:48:48 +0530

    db setup done

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// init here...
+
+import connectDB from './src/db/db.js'
+
+connectDB()
+
+
+
+// the same file approach
+/* import mongoose from "mongoose";
+import 'dotenv/config'
+import { DB_NAME } from "./src/constants.js";
+
+ ;( async() =>  {
+    try {
+       await mongoose.connect(`${process.env.DATABASE_URL}/${DB_NAME}`);
+       console.log("connected database..");
+       
+    } catch (error) {
+        console.log(error);
+        
+    }
+ })(); */

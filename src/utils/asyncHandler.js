5f296dd442dd7aa5ef64d9da22ab45182b33972d
aniketz18 <aniketzade700@gmail.com>
2026-07-15 11:55:30 +0530

    add utils

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,25 @@
+ 
+// promices use approach
+const asyncHandler = (requestHandler) => {(req, res , next) =>{
+    Promise.resolve(requestHandler(req, res, next)).catch((err) =>{
+        next(err)
+    })
+}}
+
+
+
+
+
+
+ // async approach
+ /* const asyncHandler = (fn) => async (req, res , next)=>{
+    try {
+        await fn(req , res , next);
+    } catch (error) {
+        res.status(error.code || 500).json({
+            success : false,
+            message : error.message,
+        })
+        
+    }
+ } */

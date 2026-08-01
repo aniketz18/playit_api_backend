aa994f03eda568101321159673ba0a49b8ac96f4
aniketz18 <aniketzade700@gmail.com>
2026-08-01 19:13:21 +0530

    feat(logout) : logout and authentication middleware

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -128,5 +128,30 @@ const loginUser = asyncHandler(async (req, res) => {
       )
     );
 });
-
-export { registerUser, loginUser };
+//logout user :
+//1. get cookies from --> req.body
+//2. set all cookies NULL
+//3.  logged out success
+const logoutUser = asyncHandler(async (req, res) => {
+  await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $set: {
+        refreshToken: undefined,
+      },
+    },
+    {
+      new: true,
+    }
+  );
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+  return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User Logged Out Success"));
+});
+export { registerUser, loginUser, logoutUser };
---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,29 @@
+import { User } from "../models/user.model";
+import { ApiError } from "../utils/apiErrors";
+import { asyncHandler } from "../utils/asyncHandler";
+import jwt from "jsonwebtoken";
+// 1. check is cookies is added
+// 2, if yes then check is valid
+// 3. if valid check isauthenticated if is correct tokens
+// 4. then next
+export const jwtAuth = asyncHandler(async (req, res, next) => {
+  try {
+    const token =
+      req.cookies?.accessToken ||
+      req.header("Authorization")?.replace("Bearer ", "");
+    if (!token) {
+      throw new ApiError(401, "Unauthorized request");
+    }
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    const user = await User.findById(decodedToken?._id).select(
+      "-password -refreshToken"
+    );
+    if (!user) {
+      throw new ApiError(401, "Invalid Authorization");
+    }
+    req.user = user;
+    next();
+  } catch (error) {
+    throw new ApiError(401, error?.message || "Invalid Access token");
+  }
+});
